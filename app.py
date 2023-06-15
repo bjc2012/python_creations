82ea0bd36666540aa387d57b8950d6233d8c0434
bjc2012 <brentcollins012@yahoo.com>
2023-06-15 04:54:50 +0000

    quiz code updated

---
+++ app.py
@@ -0,0 +1,29 @@
+ # This multiple choice test contains 3 NBA related questions that test your history of the game
+
+
+from quiz import Question
+
+question_prompts = [
+    "What year was the game of basketball invented?\n(a) Bob Cousy\n(b) James Naismith\n(c) Jerry Naismith",
+    "Which player has scored the most points in NBA History?\n(a) Michael Jordan\n(b) LeBron James\n(c) Kareem Abdul-Jabbar\n(d) Kobe Bryant",
+    "Which team has the most NBA championships?\n(a) Lakers\n(b) Celtics\n(c) Bulls\n(d) Lakers and Celtics are both tied for 17 championships"
+]
+
+questions = [
+    Question(question_prompts[0], "b"),
+    Question(question_prompts[1], "b"),
+    Question(question_prompts[2], "d"),
+]
+
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+            
+    print("You answered " + str(score) + "/" + str(len(questions)) + " correctly")
+
+
+run_test(questions)   
